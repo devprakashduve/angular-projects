1b2f425855ca4dc5f1b11c797239b344a6b2a4af
devprakashduve <dev786.1@gmail.com>
2019-12-11 00:21:06 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+//import { NgModule } from '@angular/core';
+//import { BrowserModule } from '@angular/platform-browser';
+//import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+//import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule
-  ],
-  declarations: [
-    AppComponent
-  ],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule ,ReactiveFormsModule ],
+  declarations: [ AppComponent],
+  bootstrap:    [ AppComponent ]
 })
-
 export class AppModule { }
